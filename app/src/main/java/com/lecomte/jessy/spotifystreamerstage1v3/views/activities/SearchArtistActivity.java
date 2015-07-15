c9081ec87151e7ecd5bd4d2b07b00c7329105422
Jessy Lecomte <jeslec2015@gmail.com>
2015-07-15 09:20:55 -0400

    Remove unused menu-related code & xml file

--- app/src/main/java/com/lecomte/jessy/spotifystreamerstage1v3/views/activities/SearchArtistActivity.java
+++ app/src/main/java/com/lecomte/jessy/spotifystreamerstage1v3/views/activities/SearchArtistActivity.java
@@ -125,21 +125,6 @@ public class SearchArtistActivity extends AppCompatActivity implements
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
